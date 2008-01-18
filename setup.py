0ed1600291784c3e45fc42d8dc064bc901bb0508
Derek Richardson <derek.richardson@gatech.edu>
2008-01-18 18:50:11 +0000

    First cut at eggifying CAS4PAS.
    git-svn-id: https://svn.plone.org/svn/collective/Products.CAS4PAS/trunk@57148 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '2.0'
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='Products.CAS4PAS',
+      version=version,
+      description='A PAS plugin that authenticates users against a CAS ' \
+                  '(Central Authentication Service) server.',
+      long_description=(
+        read('README.txt')
+        + '\n\n' +
+        read('CHANGES.txt')
+        ),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Zope2",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='zope PAS',
+      author='Lennart Regebro',
+#      author_email='',
+#      url='',
+      license='ZPL',
+      packages=find_packages(),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          # Products.PluggableAuthService is a dep, but can't be explicit in Plone 3.
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
